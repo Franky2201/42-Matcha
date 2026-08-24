37a1c7207b8bc65beca6edecd83ce24e6955b878
juhanse <julien.hanse12@gmail.com>
2026-08-24 17:10:57 +0200

    add: email for reset password

--- apps/backend/app/core/mailer.py
+++ apps/backend/app/core/mailer.py
@@ -8,3 +8,16 @@ async def send_verification_email(email: str, token: str):
         f"Body: Cliquez sur ce lien pour vérifier votre compte : \n{verification_link}"
     )
     print("------------------\n")
+
+
+async def send_password_reset_email(email: str, token: str):
+    reset_link = f"http://localhost:5173/reset-password?token={token}"
+
+    print("\n--- MOCK EMAIL ---")
+    print(f"To: {email}")
+    print("Subject: Réinitialisation de votre mot de passe Matcha")
+    print(
+        f"Body: Cliquez sur ce lien pour choisir un nouveau mot de passe : \n"
+        f"{reset_link}"
+    )
+    print("------------------\n")
