b5cf3a289e3dac3cef10a8a2b8f0a6c0e6b63719
juhanse <julien.hanse12@gmail.com>
2026-09-04 16:18:02 +0200

    add: user context

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -7,12 +7,15 @@ import { router } from "./router";
 import { AuthProvider } from './context/AuthContext.tsx'
 import { client } from './lib/apollo.ts'
 import { ApolloProvider } from '@apollo/client/react'
+import { UserProvider } from './context/UserContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ApolloProvider client={client}>
       <AuthProvider>
-        <RouterProvider router={router} />
+        <UserProvider>
+          <RouterProvider router={router} />
+        </UserProvider>
       </AuthProvider>
     </ApolloProvider>
   </StrictMode>,
